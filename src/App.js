5303962e1b8d259d01e4913316e2efdfb0ac8f17
Code-Shine <xieyuanguo1116@163.com>
2022-11-26 00:17:16 +0800

    test

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends React.Component {
   };
 
   switchTab = (type) => {
-    //实现思路: 点击了那个tab 就将其作为当前得Tab,然后把type属性交给state中active
+    //实现思路: 点击那个tab 就将其作为当前得Tab,然后把type属性交给state中active
     this.setState({
       active: type,
     });
